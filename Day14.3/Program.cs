0c051bfd6d312eeeb35f959ab57d9c324c4f950b
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-04-23 11:19:11 +0530

    add : Day 14.3 files

---
+++ Day14.3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace pratice3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+        }
+    }
+}
