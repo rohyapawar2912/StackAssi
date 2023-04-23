28e3bbe96f00d870076c4bee01259c152c12ce8d
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-04-23 11:21:53 +0530

    add : Day 14.4 files

---
+++ Day14.4/Node.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratice4
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public class Queue
+    {
+        private Node front;
+        private Node rear;
+
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (front == null)
+            {
+                front = newNode;
+                rear = newNode;
+            }
+            else
+            {
+                rear.next = newNode;
+                rear = newNode;
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (front == null)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+            int data = front.data;
+            front = front.next;
+            if (front == null)
+            {
+                rear = null;
+            }
+            return data;
+        }
+
+        public int Peek()
+        {
+            if (front == null)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+            return front.data;
+        }
+
+        public bool IsEmpty()
+        {
+            return front == null;
+        }
+
+        public void DequeueFromBeginning()
+        {
+            if (front == null)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+            front = front.next;
+            if (front == null)
+            {
+                rear = null;
+            }
+        }
+    }
+}
---
+++ Day14.4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Pratice4
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+            Console.WriteLine(queue.Dequeue()); // prints 56
+
+            queue.DequeueFromBeginning();
+
+            Console.WriteLine(queue.Dequeue()); // prints 70
+            Console.WriteLine(queue.Dequeue()); // throws InvalidOperationException
+        }
+    }
+}
