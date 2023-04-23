ed8695835493a33d363dfd3e289c9b6aa55d48e3
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-04-23 11:15:34 +0530

    add : Day 14.2 files

---
+++ Day14.2/Node.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public class Stack
+    {
+        private Node top;
+
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+            }
+        }
+
+        public int Pop()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            int data = top.data;
+            top = top.next;
+            return data;
+        }
+
+        public int Peek()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            return top.data;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+    }
+}
---
+++ Day14.2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+
+            while (!stack.IsEmpty())
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
