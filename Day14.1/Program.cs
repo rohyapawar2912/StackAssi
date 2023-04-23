66420544e71a45c901ec13437b50f45955741a8c
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-04-23 11:12:09 +0530

    add : Day 14.1 files

---
+++ Day14.1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice1
+{
+    
+        public class Program
+        {
+            public static void Main()
+            {
+                Stack stack = new Stack();
+                stack.Push(70);
+                stack.Push(30);
+                stack.Push(56);
+
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop()); 
+            }
+        }
+    
+}
